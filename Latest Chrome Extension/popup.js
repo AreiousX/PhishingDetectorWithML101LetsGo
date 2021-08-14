c8b563ae3c00f90d3db7d542ecc07ae301472dde
quek1001 <87413404+quek1001@users.noreply.github.com>
2021-08-15 00:05:25 +0800

    Add files via upload

--- Latest Chrome Extension/popup.js
+++ Latest Chrome Extension/popup.js
@@ -1,4 +1,5 @@
 
+
   chrome.runtime.onMessage.addListener(function(request, sender) {
     if (request.action == "getSource") {
       let html = request.source;
@@ -35,10 +36,63 @@
 
   function fillTable(response) {
     var table = document.getElementById('urlTable');
-    for (var i = 0; i < response.length; i++) {
+    var counter = document.getElementById('urlCounter');
+    var totalCount = 0;
+    var maliciousCount = 0;
+    var uri = [];
+    const rString = ["Malicious ads on website","Link was found in database","Website was found to collect sensitive information","Sent from ap public email address","Invalid domain name","Domain Name is misspelt","Url is too long"];
+    for (var i = 0; i < response.length; i++) {   
         var cell = formTableforDisplay(response[i]);
+        totalCount++;
+        if (response[i].result == 1){
+          maliciousCount++;
+        }
         table.insertAdjacentHTML('beforeEnd', cell);
+
+        uri.push(document.getElementsByClassName('maliciousLink')[i].innerHTML);
+        // alert(document.getElementsByClassName('maliciousLink')[i].innerHTML)
+
     }
+    var countText = maliciousCount/totalCount * 100;
+    countText = countText.toFixed(0);
+    if (countText >= 30){
+      counterText = '<a style="font-size:15px;">This email is</a> <b><a style="color:#ff5454; font-size:20px;">MALICIOUS</a></b><a>.</a> <p style="font-size:15px;"> ' + maliciousCount + '/' + totalCount + ' (' + countText + '%) of URLs has been detected as malicious.</p>'
+      counter.insertAdjacentHTML('beforeend', counterText)
+    }
+    else if (countText >= 20){
+      counterText = '<a style="font-size:15px;">This email is</a> <b><a style="color:#ffb554; font-size:20px;">POTENTIALLY MALICIOUS</a></b><a>.</a> <p style="font-size:15px;"> ' + maliciousCount + '/' + totalCount + ' (' + countText + '%) of URLs has been detected as malicious.</p>'
+      counter.insertAdjacentHTML('beforeend', counterText)
+    }
+    else{
+      counterText = '<a style="font-size:15px;">This email is</a> <b><a style="color:lightgreen; font-size:20px;">SAFE</a></b><a>.</a> <p style="font-size:15px;"> ' + maliciousCount + '/' + totalCount + ' (' + countText + '%) of URLs has been detected as malicious.</p>'
+      counter.insertAdjacentHTML('beforeend', counterText)
+    }
+    let buttons = document.querySelectorAll('.testingLink');
+    buttons.forEach((btn,index) => { btn.addEventListener("click", 
+    function()
+    {
+      
+      
+      const ipRegex = /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b/;
+      const digitRegex = /\d{10}(\d+)?$/;
+      const directoryRegex = /\/\w{15,}\b/;
+
+      if(uri[index].match(ipRegex))
+        {
+          alert("IP address was found in");
+        }
+      else if(uri[index].match(digitRegex))
+      {
+        alert("URL contains a long string of numbers");
+      }
+      else if(uri[index].match(directoryRegex))
+      {
+        alert("Directory is very long string")
+      }
+
+    }
+
+    ); });
 }
 
   function formTableforDisplay(content) {
@@ -49,17 +103,34 @@
     var prediction = content.result;
     if (prediction == 1){
       prediction = "Malicious";
+
     }
     else{
       prediction = "Safe";
     }
-
-    var cell =
-        '<tr><td class="col0">' + url + '</td>' +
-        '<td class="col1">' + prediction + '</td>' +
-        '<td class="col1">' + probability + '%</td></tr>'
-
+    if (prediction == "Malicious"){
+      if (probability <= 75){
+        var cell =
+        '<tr><td class="col0 maliciousLink" style="background-color:#ffb554">' + url + '</td>' +
+        '<td class="col1" style="background-color:#ffb554">' + prediction + '</td>' +
+        '<td class="col1" style="background-color:#ffb554">' + probability + '%</td>' +
+        '<td class="col1"><button class="testingLink">?</button></td></tr>'
+      }
+      else {
+        var cell =
+        '<tr><td class="col0 maliciousLink" style="background-color:#ff5454">' + url + '</td>' +
+        '<td class="col1" style="background-color:#ff5454">' + prediction + '</td>' +
+        '<td class="col1" style="background-color:#ff5454">' + probability + '%</td>'+
+        '<td class="col1"><button class="testingLink">?</button></td></tr>'
+      }
+    }
+    else{  
+      var cell =
+        '<tr><td class="col0" style="background-color:lightgreen">' + url + '</td>' +
+        '<td class="col1" style="background-color:lightgreen">' + prediction + '</td>' +
+        '<td class="col1" style="background-color:lightgreen">' + probability + '%</td></tr>'
+    }
     return cell;
 }
-  
+
   window.onload = onWindowLoad;
